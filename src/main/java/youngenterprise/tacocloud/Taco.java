26584879b619fe9003d503d877ae375354f87698
Nakaw <nakawyoung@gmail.com>
2021-12-03 13:26:28 -0500

    Initial commit, completed order form and homepage order, as well as payment page, just need backend

---
+++ src/main/java/youngenterprise/tacocloud/Taco.java
@@ -0,0 +1,20 @@
+package youngenterprise.tacocloud;
+
+import java.util.List;
+// end::allButValidation[]
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+// tag::allButValidation[]
+import lombok.Data;
+
+@Data
+public class Taco {
+    @NotNull
+    @Size(min=5,message="Name must be at least 5 characters long")
+    private String name;
+    @Size(min=1,message="You must choose at least 1 ingredient")
+    private List<String>ingredients;
+
+}
+//end::allButValidation[]
+//tag::end[]
