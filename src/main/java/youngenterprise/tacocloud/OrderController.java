be1acf0822983b84bbb80937bb4f51900189fade
Nakaw <nakawyoung@gmail.com>
2021-12-08 21:44:38 -0500

    Trying to force install maven dependencies to fix sessionstatus issue

--- src/main/java/youngenterprise/tacocloud/OrderController.java
+++ src/main/java/youngenterprise/tacocloud/OrderController.java
@@ -7,24 +7,35 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import youngenterprise.tacocloud.data.OrderRepository;
 
 import javax.validation.Valid;
 
-@Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo){
+        this.orderRepo=orderRepo;
+    }
+
     @GetMapping("/current")
     public String orderForm(Model model){
-        model.addAttribute("order",new Order());
         return "orderForm";
     }
+
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors){
+    public String processOrder(@Valid Order order, Errors errors, SesssionStatus sessionStatus){
         if(errors.hasErrors()){
             return "orderForm";
         }
-        log.info("Order submitted: "+order);
+
+        orderRepo.save(order);
+        sessionStatus.setComplete();
         return "redirect:/";
     }
 }
