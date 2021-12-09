c3dec757c18505f08eebc7a04aa7cb393924a549
Nakaw <nakawyoung@gmail.com>
2021-12-09 13:25:39 -0500

    Adding JPA persistence to Order and Taco

--- src/main/java/youngenterprise/tacocloud/Taco.java
+++ src/main/java/youngenterprise/tacocloud/Taco.java
@@ -3,21 +3,34 @@ package youngenterprise.tacocloud;
 import java.util.Date;
 import java.util.List;
 // end::allButValidation[]
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 // tag::allButValidation[]
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
+
     private Date createdAt;
 
     @NotNull
     @Size(min=5,message="Name must be at least 5 characters long")
     private String name;
+
+    @ManyToMany(targetEntity = Ingredient.class)
     @Size(min=1,message="You must choose at least 1 ingredient")
     private List<Ingredient>ingredients;
+
+    @PrePersist
+    void createdAt(){
+        this.createdAt=new Date();
+    }
 
 }
 //end::allButValidation[]
