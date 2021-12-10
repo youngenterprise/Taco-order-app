7261728931b94c009fc527092c3aa29829dd4af1
Nakaw <nakawyoung@gmail.com>
2021-12-10 16:00:18 -0500

    Adding user security

--- src/main/java/youngenterprise/tacocloud/security/SecurityConfig.java
+++ src/main/java/youngenterprise/tacocloud/security/SecurityConfig.java
@@ -4,16 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
+    {/* @Override
     protected void configure(AuthenticationManagerBuilder auth)throws Exception{
         auth.inMemoryAuthentication().withUser("buzz").password("infinity")
                 .authorities("ROLE_USER").and().withUser("woody").password("bullseye")
-                 .authorities("ROLE_USER");
+                .authorities("ROLE_USER");
     }
 
     @Override
@@ -21,6 +22,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.jdbcAuthentication().dataSource(dataSource)
                 .usersByUsernameQuery("select username, password,enabled from Users "+ "where username=?")
                 .authoritiesByUsernameQuery("select username, authority from UserAuthorities "+
-                        "where username=?");
+                        "where username=?").passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+    } */}
+
+
+    @Override
+    protected void configure (AuthenticationManagerBuilder auth)throws Exception{
+        auth.ldapAuthentication().userSearchFilter("(uid={0})")
+                .groupSearchFilter("member={0}")
+                .groupSearchBase("ou=groups").groupSearchFilter("member={0}")
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("passcode").contextSource().root("dc=tacocloud,dc=com");
     }
 }
