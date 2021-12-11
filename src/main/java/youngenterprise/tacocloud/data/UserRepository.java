afc26ff41ef7694898e762d7c0068b0fee059fdb
Nakaw <nakawyoung@gmail.com>
2021-12-11 18:21:07 -0500

    Added user repo

--- src/main/java/youngenterprise/tacocloud/data/UserRepository.java
+++ src/main/java/youngenterprise/tacocloud/data/UserRepository.java
@@ -1,4 +1,8 @@
 package youngenterprise.tacocloud.data;
 
-public class UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import youngenterprise.tacocloud.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findbyUsername(String username);
 }
