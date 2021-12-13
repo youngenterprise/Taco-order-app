520dc1fdcf3a8c12a9b8c94c76525b5a6b110ba5
Nakaw <nakawyoung@gmail.com>
2021-12-13 15:47:15 -0500

    Finished fixing the bug in the UserRepositoryUserDetailsService

--- src/main/java/youngenterprise/tacocloud/security/UserRepositoryUserDetailsService.java
+++ src/main/java/youngenterprise/tacocloud/security/UserRepositoryUserDetailsService.java
@@ -14,40 +14,25 @@ import youngenterprise.tacocloud.data.UserRepository;
 
 
 @Service
-public class UserRepositoryUserDetailsService implements UserDetailsService {
+public class UserRepositoryUserDetailsService
+        implements UserDetailsService {
+
     private UserRepository userRepo;
 
     @Autowired
-    public UserRepositoryUserDetailsService(UserRepository userRepo){
-        this.userRepo=userRepo;
+    public UserRepositoryUserDetailsService(UserRepository userRepo) {
+        this.userRepo = userRepo;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user=userRepo.findbyUsername(username);
-        if(user!=null){
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepo.findbyUsername(username);
+        if (user != null) {
             return user;
         }
-        throw new UsernameNotFoundException("User '"+username+"' not found");
+        throw new UsernameNotFoundException(
+                "User '" + username + "' not found");
     }
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Override
-    protected void configure (AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userDetailsService);
-    }
-
-    @Bean
-    public PasswordEncoder encoder() {
-        return new StandardPasswordEncoder("53cr3t");
-    }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(encoder());
-    }
 }
