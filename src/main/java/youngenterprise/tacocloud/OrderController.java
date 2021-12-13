9bba23d192edc62207255bf72581ddfca4a2843f
Nakaw <nakawyoung@gmail.com>
2021-12-13 15:43:19 -0500

    Injected user into order controller

--- src/main/java/youngenterprise/tacocloud/OrderController.java
+++ src/main/java/youngenterprise/tacocloud/OrderController.java
@@ -1,6 +1,7 @@
 package youngenterprise.tacocloud;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -29,11 +30,12 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus){
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user){
         if(errors.hasErrors()){
             return "orderForm";
         }
-
+        order.setUser(user);
         orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
