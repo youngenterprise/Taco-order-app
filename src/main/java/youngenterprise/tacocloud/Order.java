5d34b90f15e8cf536af760a3b14c9d5be7f4cd1b
Nakaw <nakawyoung@gmail.com>
2021-12-13 14:30:26 -0500

    Made a change to Order to connect it to User entity

--- src/main/java/youngenterprise/tacocloud/Order.java
+++ src/main/java/youngenterprise/tacocloud/Order.java
@@ -50,6 +50,9 @@ public class Order implements Serializable {
         this.tacos.add(design);
     }
 
+    @ManyToOne
+    private User user;
+
     @PrePersist
     void placedAt(){
         this.placedAt=new Date();
